83d467bfaf87e9715fa3e680c5e1a9c8f8c0eb35
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-06-27 14:36:55 +0300

    created serializer, view and url for Route model

--- airport/urls.py
+++ airport/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import AirportViewSet
+from airport.views import AirportViewSet, RouteViewSet
 
 app_name = "airport"
 
 router = routers.DefaultRouter()
 router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
