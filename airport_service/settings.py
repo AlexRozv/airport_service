e10a5fa0e159017bea6f3fb9397b34b5b30dfeb2
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-06-27 17:44:00 +0300

    added custom permission class

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -52,6 +52,9 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "user.permissions.IsAdminOrIfAuthenticatedReadOnly",
     )
 }
 
