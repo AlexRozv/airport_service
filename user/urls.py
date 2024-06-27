83d5cc5a8fdbbdd9586518b9cab1b25137b8efa6
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-06-27 17:37:39 +0300

    added User serializer and registration

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+from user.views import CreateUserView
 
 app_name = "user"
 
 urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
