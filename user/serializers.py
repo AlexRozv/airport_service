1d82b28debbedf332b29544d0842525e30b2f3ac
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-07-03 15:02:33 +0300

    changed user to use email as login instead of username

--- user/serializers.py
+++ user/serializers.py
@@ -1,16 +1,16 @@
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = settings.AUTH_USER_MODEL
-        fields = ("id", "username", "email", "password", "is_staff")
+        model = get_user_model()
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        return settings.AUTH_USER_MODEL.objects.create_user(**validated_data)
+        return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
@@ -18,4 +18,5 @@ class UserSerializer(serializers.ModelSerializer):
         if password:
             user.set_password(password)
             user.save()
+
         return user
